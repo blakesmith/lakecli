0fd03c2539227929671d44f7a922c587dac8aa11
Blake Smith <blakesmith0@gmail.com>
2024-09-21 20:54:29 -0500

    Read parquet files directly as well

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 use clap::{Parser, Subcommand};
-use deltalake::{datafusion::prelude::SessionContext, kernel::StructType};
-use std::sync::Arc;
+use deltalake::{
+    datafusion::{error::DataFusionError, prelude::SessionContext},
+    kernel::StructType,
+    DeltaTable, DeltaTableError,
+};
+use std::{ffi::OsStr, path::Path, sync::Arc};
+use thiserror::Error;
 
 #[derive(Parser)]
 #[command(author, version, about, long_about)]
@@ -22,6 +27,138 @@ fn print_schema(schema: &StructType) {
     }
 }
 
+#[derive(Debug)]
+enum Table {
+    Delta(Arc<DeltaTable>),
+    Parquet { table_path: Box<str> },
+}
+
+impl Table {
+    pub async fn files(&self) -> Result<(), Error> {
+        match self {
+            Table::Delta(delta_table) => {
+                let files: Vec<_> = delta_table.get_file_uris()?.collect();
+                println!("files: {:?}", files);
+            }
+            other => {
+                println!("'files' call unsupported for: {:?}", other);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn schema(&self) -> Result<(), Error> {
+        match self {
+            Table::Delta(delta_table) => match delta_table.schema() {
+                Some(schema) => print_schema(schema),
+                None => {
+                    println!("No schema found in delta table!");
+                }
+            },
+            Table::Parquet { table_path: _ } => todo!(),
+        }
+
+        Ok(())
+    }
+
+    pub async fn version(&self) -> Result<(), Error> {
+        match self {
+            Table::Delta(delta_table) => {
+                println!("version: {}", delta_table.version());
+            }
+            other => {
+                println!("'version' call unsupported for: {:?}", other);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn metadata(&self) -> Result<(), Error> {
+        match self {
+            Table::Delta(delta_table) => {
+                println!("metadata: {:?}", delta_table.metadata()?);
+            }
+            other => {
+                println!("'metadata' call unsupported for: {:?}", other);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn history(&self, limit: Option<usize>) -> Result<(), Error> {
+        match self {
+            Table::Delta(delta_table) => {
+                let history = delta_table.history(limit.clone()).await?;
+                println!("history:");
+                for commit in history {
+                    println!("{:?}", commit);
+                }
+            }
+            other => {
+                println!("'history' call unsupported for: {:?}", other);
+            }
+        }
+
+        Ok(())
+    }
+
+    pub async fn query(&self, ctx: &SessionContext, query: &str) -> Result<(), Error> {
+        self.register_table(ctx).await?;
+        let dataframe = ctx.sql(&query).await?;
+        dataframe.show().await?;
+        Ok(())
+    }
+
+    async fn register_table(&self, ctx: &SessionContext) -> Result<(), Error> {
+        match self {
+            Table::Delta(delta_table) => {
+                let metadata = delta_table.metadata()?;
+                if let Some(table_name) = &metadata.name {
+                    ctx.register_table(table_name, delta_table.clone())?;
+                }
+                // Always register table 't', for simplicity
+                ctx.register_table("t", delta_table.clone())?;
+            }
+            Table::Parquet { table_path } => {
+                ctx.register_parquet("t", table_path, Default::default())
+                    .await?;
+            }
+        }
+
+        Ok(())
+    }
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Unknown table format")]
+    UnknownTableFormat,
+
+    #[error("Delta table error")]
+    Delta(#[from] DeltaTableError),
+
+    #[error("Data fusion error")]
+    DataFusion(#[from] DataFusionError),
+}
+
+async fn open_table(table_name: &str) -> Result<Table, Error> {
+    let extension = Path::new(table_name).extension().and_then(OsStr::to_str);
+    match extension {
+        Some("parquet") => Ok(Table::Parquet {
+            table_path: table_name.into(),
+        }),
+        Some(_other) => Err(Error::UnknownTableFormat),
+        None => {
+            let table =
+                deltalake::open_table_with_storage_options(table_name, Default::default()).await?;
+            Ok(Table::Delta(Arc::new(table)))
+        }
+    }
+}
+
 #[derive(Subcommand)]
 enum Commands {
     #[clap(about = "List files in the table")]
@@ -39,7 +176,9 @@ enum Commands {
     #[clap(about = "Show table schema")]
     Schema { table: String },
 
-    #[clap(about = "Print the current / latest table version number")]
+    #[clap(
+        about = "Print the current / latest table version number. Currently for delta tables only"
+    )]
     Version { table: String },
 
     #[clap(about = "Query the table with a DataFusion query")]
@@ -54,49 +193,30 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     match &cli.command {
         Commands::Files { table } => {
-            let table = deltalake::open_table(&table).await?;
-            let files: Vec<_> = table.get_file_uris()?.collect();
-            println!("files: {:?}", files);
+            let table = open_table(&table).await?;
+            table.files().await?;
         }
         Commands::Schema { table } => {
-            let table = deltalake::open_table(&table).await?;
-            match table.schema() {
-                Some(schema) => print_schema(schema),
-                None => {
-                    println!("No schema found in delta table!");
-                }
-            }
+            let table = open_table(&table).await?;
+            table.schema().await?;
         }
         Commands::Version { table } => {
-            let table = deltalake::open_table(&table).await?;
-            println!("version: {}", table.version());
+            let table = open_table(&table).await?;
+            table.version().await?;
         }
         Commands::Metadata { table } => {
-            let table = deltalake::open_table(&table).await?;
-            println!("metadata: {:?}", table.metadata()?);
+            let table = open_table(&table).await?;
+            table.metadata().await?;
         }
         Commands::History { table, limit } => {
-            let table = deltalake::open_table(&table).await?;
-            let history = table.history(limit.clone()).await?;
-            println!("history:");
-            for commit in history {
-                println!("{:?}", commit);
-            }
+            let table = open_table(&table).await?;
+            table.history(*limit).await?;
         }
         Commands::Query { table, query } => {
-            println!("Executing query: {}", query);
-            let table = Arc::new(deltalake::open_table(&table).await?);
+            let table_name = table;
+            let table = open_table(&table_name).await?;
             let ctx = SessionContext::new();
-            let metadata = table.metadata()?;
-            if let Some(table_name) = &metadata.name {
-                ctx.register_table(table_name, table.clone())
-                    .expect("Failed to register table");
-            }
-            // Always register table 't', for simplicity
-            ctx.register_table("t", table.clone())
-                .expect("Failed to register table");
-            let dataframe = ctx.sql(&query).await?;
-            dataframe.show().await?
+            table.query(&ctx, query).await?;
         }
     }
 
